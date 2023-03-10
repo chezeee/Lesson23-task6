da71715a8ef5b21024915fabdb953b28f0c67209
Ilya Yalymov <chezeee@gmail.com>
2023-03-10 21:28:07 +0300

    Solution added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  // your code
+  const shoppingList = document.querySelector("#shopping-list");
+
+  fetch(
+    "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/shoppinglist/items.json"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Something went wrong...");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((purchaseData) => {
+        shoppingList.insertAdjacentHTML(
+          "beforeend",
+          `<li>${purchaseData.item}</li>`
+        );
+      });
+    })
+    .catch((error) => console.error(error));
 };
 
 // Sample usage - do not modify
